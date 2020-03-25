788f3d729fc45a6aee038666737724a333536bd2
viy-777 <viy-777@ya.ru>
2020-03-25 13:45:36 +0500

    41 и 42

---
+++ src/ru/baranov/task_4_1.java
@@ -0,0 +1,25 @@
+package ru.baranov;
+
+//Написать программу для поиска минимального из двух чисел.
+
+import java.util.Scanner;
+
+public class task_4_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Программа для поиска минимального из двух чисел");
+
+        int num1 = 0, num2 = 0;
+
+        System.out.println("Введите первое число:");   //ввод пользователем числа №1
+        Scanner in = new Scanner(System.in);
+        num1 = in.nextInt();
+
+        System.out.println("Введите второе число:");   //ввод пользователем числа №2
+        num2 = in.nextInt();
+
+        if (num1 > num2) System.out.println("Первое число больше второго");
+        if (num1 < num2) System.out.println("Первое число меньше второго");
+        if (num1 == num2) System.out.println("Первое число равно второму");
+}
+}
