90c65f1f092e142596e0eff3fb54a458126e1732
viy-777 <viy-777@ya.ru>
2020-03-29 22:25:46 +0500

    new

---
+++ src/ru/baranov/task6/Main.java
@@ -0,0 +1,22 @@
+package ru.baranov.task6;
+
+class University {
+    String name;
+    int number_faculties;
+}
+
+class Faculty {
+    String name;
+    int number_specialties;
+}
+
+class  Specialty {
+    String name;
+    int number_groups;
+}
+
+public class Main {
+    public static void main (String[] args){
+
+    }
+}
