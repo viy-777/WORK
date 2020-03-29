5770a43cce842329552424e570a324da626a0fc6
viy-777 <viy-777@ya.ru>
2020-03-29 23:31:57 +0500

    new

--- src/ru/baranov/task6/Main.java
+++ src/ru/baranov/task6/Main.java
@@ -1,13 +1,22 @@
 package ru.baranov.task6;
 
 class University {
-    String name;
+    String name = "МГУ";
     int number_faculties;
+
+    University(String name)
+    {
+        this.name = name;
+    }
 }
 
 class Faculty {
     String name;
     int number_specialties;
+
+    public void rating() {
+        System.out.println("№1");
+    }
 }
 
 class  Specialty {
@@ -17,6 +26,12 @@ class  Specialty {
 
 public class Main {
     public static void main (String[] args){
+Faculty x;
+x = new Faculty();
+x.name = "Приборостроение";
+x.number_specialties = 16;
+x.rating();
 
+        System.out.println("Название факультета: '" + x.name + "', кол-во специальностей: " + x.number_specialties);
     }
 }
