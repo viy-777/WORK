fbf4ad9c4d94f7718398221c446d49f08d32c5de
viy-777 <viy-777@ya.ru>
2020-04-06 09:46:58 +0500

    new

---
+++ src/ru/baranov/task9/NumberFirst.java
@@ -0,0 +1,57 @@
+package ru.baranov.task9;
+
+//Написать абстрактный класс Animal с абстрактным методом getName.
+//Сделать несколько классов животных, наследников Animal.
+//Метод getName должен выводит название каждого животного.
+
+abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void display();
+}
+
+class Animal1 extends Animal {
+    private String name1;
+
+    public Animal1(String name) {
+        super(name);
+        this.name1 = name;
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Животное1: %s \n", super.getName());
+    }
+}
+
+class Animal2 extends Animal {
+    private String name2;
+
+    public Animal2(String name) {
+        super(name);
+        this.name2 = name;
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Животное2: %s \n", super.getName());
+    }
+}
+
+public class NumberFirst {
+    public static void main(String[] args) {
+        Animal1 a1 = new Animal1("Тигр");
+        Animal2 a2 = new Animal2("Леопард");
+
+        a1.display();
+        a2.display();
+    }
+}
