aca142e2ccd129222030df4668f28cee09b1e329
viy-777 <viy-777@ya.ru>
2020-04-07 11:51:19 +0500

    new

--- src/ru/baranov/task9/NumberThird.java
+++ src/ru/baranov/task9/NumberThird.java
@@ -5,10 +5,14 @@ package ru.baranov.task9;
 
 interface Run_ {
     void running();
+
+    void chemp_running();
 }
 
 interface Swim_ {
     void swimming();
+
+    void chemp_swimming();
 }
 
 class Man implements Run_, Swim_ {
@@ -24,8 +28,16 @@ class Man implements Run_, Swim_ {
         System.out.printf("Бегун с именем %s (возраст %d) \n", name, age);
     }
 
+    public void chemp_running() {
+        System.out.printf("Чемпион по бегу %s (возраст %d) \n", name, age);
+    }
+
     public void swimming() {
         System.out.printf("Пловец с именем %s (возраст %d) \n", name, age);
+    }
+
+    public void chemp_swimming() {
+        System.out.printf("Чемпион по плаванию %s (возраст %d) \n", name, age);
     }
 
 }
@@ -35,7 +47,13 @@ public class NumberThird {
         Man m1 = new Man("Егор", 44);
         m1.running();
 
-        Man m2 = new Man("Сергей", 24);
-        m2.swimming();
+        Man m2 = new Man("Татьяна", 23);
+        m2.chemp_running();
+
+        Man m3 = new Man("Джон", 30);
+        m3.swimming();
+
+        Man m4 = new Man("Сергей", 24);
+        m4.chemp_swimming();
     }
 }
