d394479e98f2a593c2dfccc9fa27c93e8f9a5533
viy-777 <viy-777@ya.ru>
2020-04-28 21:21:44 +0500

    выполнен task8

---
+++ src/ru/baranov/task8/num4/Converted.java
@@ -0,0 +1,10 @@
+package ru.baranov.task8.num4;
+
+public class Converted {
+
+    public static void conv() {
+        Statement.setNumber(Contract.getNumber());
+        Statement.setDate(Contract.getDate());
+        Statement.setContain(Contract.getContain());
+    }
+}
