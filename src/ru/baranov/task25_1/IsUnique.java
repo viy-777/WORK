6f89256072b6eb635b517d3e5a648fd87fa25909
viy-777 <viy-777@ya.ru>
2020-06-16 15:23:03 +0500

    внесение правок в task25-1

--- src/ru/baranov/task25_1/IsUnique.java
+++ src/ru/baranov/task25_1/IsUnique.java
@@ -5,19 +5,12 @@ import java.util.*;
 public class IsUnique {
 
     public boolean isUnique(Map<String, String> map) {
-
-        int count = 0;
-
         Set<String> setNames = new HashSet<>();
-        for (Map.Entry<String, String> doubleName : map.entrySet()) {
-            String name = doubleName.getValue();
-            if (setNames.contains(name))
-                count++;
-            else {
+        for (String name : map.values()) {
+            if (setNames.contains(name)) return false;
+            else
                 setNames.add(name);
-            }
         }
-
-        return (count < 1) || map.isEmpty();
+        return true;
     }
 }
